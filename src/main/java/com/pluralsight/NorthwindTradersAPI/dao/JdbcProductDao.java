6700acc1198363fdf81cb364c837f3f625167d9a
eriinys <eriinys@duck.com>
2025-12-09 10:17:40 -0600

    Created JdbcProductDao with constructor injected DataSource and JDBC query methods

---
+++ src/main/java/com/pluralsight/NorthwindTradersAPI/dao/JdbcProductDao.java
@@ -0,0 +1,73 @@
+package com.pluralsight.NorthwindTradersAPI.dao;
+
+import com.pluralsight.NorthwindTradersAPI.models.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class JdbcProductDao implements ProductDao{
+    private DataSource dataSource;
+
+    @Autowired
+    public JdbcProductDao(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public List<Product> getAll() {
+        List<Product> productList = new ArrayList<>();
+        String sql = "SELECT ProductID, ProductName, CategoryID, UnitPrice FROM products";
+
+        try(Connection conn = dataSource.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery()){
+
+            while(rs.next()){
+                int productId = rs.getInt("ProductID");
+                String productName = rs.getString("ProductName");
+                int categoryId = rs.getInt("CategoryID");
+                double unitPrice = rs.getDouble("UnitPrice");
+
+                productList.add(new Product(productId, productName, categoryId,unitPrice));
+            }
+            return productList;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public Product getById(int id) {
+        String sql = "SELECT ProductID, ProductName, CategoryID, UnitPrice FROM products " +
+                "WHERE ProductID = ?";
+
+        try(Connection conn = dataSource.getConnection();
+        PreparedStatement ps = conn.prepareStatement(sql)){
+            ps.setInt(1, id);
+
+            try(ResultSet rs = ps.executeQuery()){
+                if (rs.next()){
+                    return new Product(
+                            rs.getInt("ProductID"),
+                            rs.getString("ProductName"),
+                            rs.getInt("CategoryID"),
+                            rs.getDouble("UnitPrice")
+                    );
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
